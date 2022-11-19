9f87f0eb58b6609a529e0d8e48a9b887b639f806
Erika Jane Reyes <erikajanetreyes@iskolarngbayan.pup.edu.ph>
2022-11-19 17:28:57 +0800

    Add task bar/button

--- ToDoListApp.py
+++ ToDoListApp.py
@@ -25,15 +25,18 @@ set_topimg = Label(root, image=top_img).place(x=20,y=10)
 heading = Label(root, text="MY TO-DO LIST", font="arial 18 bold", fg="black", bg="#C7A15B")
 heading.place(x=115,y=35)
 
-
-
-
-
-
-
-
-
 # Add task
+
+frame = Frame(root, width=400, height=50, bg="white")
+frame.place(x=0, y=180)
+
+todo = StringVar()
+todo_entry = Entry(frame, width=18, font="arial 20", bd=0)
+todo_entry.place(x=10, y=7)
+
+button = Button(frame, text="ADD", font="arial 20 bold", width=6, bg="#FF86C3", fg="black", bd=0)
+button.place(x=300,y=0)
+
 
 # Delete task
 
