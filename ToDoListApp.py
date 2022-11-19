4a9045f0a98e8b96967cabbe8d69bd9ff65c68a7
Erika Jane Reyes <erikajanetreyes@iskolarngbayan.pup.edu.ph>
2022-11-19 20:10:54 +0800

    GUI Design Changes

--- ToDoListApp.py
+++ ToDoListApp.py
@@ -49,29 +49,34 @@ bg_img = root.configure(background="#F9E7E5")
 
 # Top Bar
 top_img = PhotoImage(file="images/topbar.png")
-set_topimg = Label(root, image=top_img).place(x=20,y=10)
-
-heading = Label(root, text="MY TO-DO LIST", font="arial 18 bold", fg="black", bg="#C7A15B")
-heading.place(x=115,y=35)
+set_topimg = Label(root, image=top_img).place(x=-53,y=10)
+todo_img = PhotoImage(file="images/todo.png")
+set_todimg = Label(root, image=todo_img).place(x=5,y=50)
+me_img = PhotoImage(file="images/me.png")
+set_meimg = Label(root, image=me_img).place(x=335,y=50)
 
 # Add task 
 frame = Frame(root, width=400, height=50, bg="white")
 frame.place(x=0, y=180)
 
 todo = StringVar()
-todo_entry = Entry(frame, width=18, font="arial 20", bd=0)
+todo_entry = Entry(frame, width=18, font=("comic sans ms", 18), bd=0)
 todo_entry.place(x=10, y=7)
 todo_entry.focus()
 
-button = Button(frame, text="ADD", font="arial 20 bold", width=6, bg="#FF86C3", fg="black", bd=0, command=addTask)
-button.place(x=300,y=0)
+add_button_fnt = ("Comic Sans MS", 20, "bold")
+add_button = Button(frame, text="Add", width=6, bg="#9F516D", fg="black", bd=0, command=addTask)
+add_button.place(x=300, y=-7)
+add_button.configure(font=add_button_fnt)
 
 # To Do List Container
-frame1 = Frame(root, bd=1, width=700, height=280,bg="#A82D6B")
+frame1 = Frame(root, bd=1, width=700, height=280,bg="#E4A1B9")
 frame1.pack(pady=(240,0))
 
-todolist_container = Listbox(frame1, font=('arial',12), width=40,height=16,bg="#A82D6B",fg="pink", cursor="hand2", selectbackground="black")
+todolist_container = Listbox(frame1, font=('Comic Sans Ms',15), width=40,height=10,bg="#E4A1B9",fg="pink", cursor="hand2", selectbackground="black")
 todolist_container.pack(side=LEFT, fill=BOTH, padx=2)
+
+
 scrollbar = Scrollbar(frame1)
 scrollbar.pack(side=RIGHT, fill=BOTH)
 
@@ -80,15 +85,17 @@ scrollbar.config(command=todolist_container.yview)
 
 # Delete task icon
 delete_icon = PhotoImage(file="images/delete.png")
-Button(root, image=delete_icon, bd=0, command=deleteTask).place(x=120, y=560)
+Button(root, image=delete_icon, bd=0, command=deleteTask).place(x=105, y=550)
+delete_text = Label(root, text="Delete", font=("Comic Sans Ms", 12), bg="#F9E7E5").place(x=105, y=605)
 
 # Save task icon
 save_icon = PhotoImage(file="images/save.png")
-Button(root, image=save_icon, bd=0, command=saveTasks).place(x=180, y=560)
+Button(root, image=save_icon, bd=0, command=saveTasks).place(x=180, y=550)
+save_text = Label(root, text="Save", font=("Comic Sans Ms", 12), bg="#F9E7E5").place(x=187, y=605)
 
 # Load task icon
 load_icon = PhotoImage(file="images/load.png")
-Button(root, image=load_icon, bd=0, command=loadTasks).place(x=240, y=560)
-
+Button(root, image=load_icon, bd=0, command=loadTasks).place(x=255, y=550)
+load_text = Label(root, text="Load", font=("Comic Sans Ms", 12), bg="#F9E7E5").place(x=263, y=605)
 
 root.mainloop()
