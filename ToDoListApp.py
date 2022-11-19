f1bf522e445f123b096b8b617a8cfc50acdc2a46
Erika Jane Reyes <erikajanetreyes@iskolarngbayan.pup.edu.ph>
2022-11-19 16:12:32 +0800

    Setting up the GUI

--- ToDoListApp.py
+++ ToDoListApp.py
@@ -1,2 +1,18 @@
 # Programmed by Erika Jane T. Reyes
 # To Do List Application Using Python GUI Tkinter
+
+import tkinter as tk
+
+root = tk.Tk()
+root.title("To-Do-List")
+root.geometry("400x650+400+100")
+root.resizable(False,False)
+
+
+# Add task
+
+# Delete task
+
+
+
+root.mainloop()
