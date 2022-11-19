1e22e6bb58231ca7d701df76214d8af69cfaac25
Erika Jane Reyes <erikajanetreyes@iskolarngbayan.pup.edu.ph>
2022-11-19 17:20:49 +0800

    Set the top/heading

--- ToDoListApp.py
+++ ToDoListApp.py
@@ -16,12 +16,23 @@ image_icon = PhotoImage(file="images/todolisticon.png")
 root.iconphoto(False,image_icon)
 
 # Window Background
-bg_img = PhotoImage(file="images/bg.png") 
-set_bgimg = Label(root, image=bg_img)
-set_bgimg.pack()
+bg_img = root.configure(background="#F9E7E5")
 
 # Top Bar
-# top_image = PhotoImage(file=)
+top_img = PhotoImage(file="images/topbar.png")
+set_topimg = Label(root, image=top_img).place(x=20,y=10)
+
+heading = Label(root, text="MY TO-DO LIST", font="arial 18 bold", fg="black", bg="#C7A15B")
+heading.place(x=115,y=35)
+
+
+
+
+
+
+
+
+
 # Add task
 
 # Delete task
